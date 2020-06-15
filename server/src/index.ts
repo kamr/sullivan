da6aa11ee9d4162698c8ba9f2b3ae53730fc9c84
Kamran Khan <kkhan@palantir.com>
2020-06-15 02:33:10 -0400

    init

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.resolve(__dirname, "..", "..", "client", "dist")));
+app.use(express.static(path.join(__dirname, "..", "..", "client", "dist")));
 
 // Create HTTP & WebSocket servers
 const server = http.createServer(app);
