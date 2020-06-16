c1a3bcfe587ad382a8b68bdad630bb2af62e67d2
Kamran Khan <kkhan@palantir.com>
2020-06-16 02:21:37 -0400

    log mongo

--- src/server/index.ts
+++ src/server/index.ts
@@ -29,6 +29,7 @@ export let STATIC_DIR: string;
 /**
  * Connect to MongoDB
  */
+console.log("mongo", process.env.MONGODB_URI)
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/colyseus', {
   autoIndex: true,
   useCreateIndex: true,
