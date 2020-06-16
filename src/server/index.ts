57f7d290e1ba00708c19538ea6d9d2b45227788c
Kamran Khan <kkhan@palantir.com>
2020-06-16 00:58:48 -0400

    lib/public

--- src/server/index.ts
+++ src/server/index.ts
@@ -55,7 +55,7 @@ if (process.env.NODE_ENV !== "production") {
 
 } else {
     // on production, use ./public as static root
-    STATIC_DIR = path.resolve(__dirname, "public");
+    STATIC_DIR = path.resolve(__dirname, "/lib/public/");
 }
 
 app.use("/", express.static(STATIC_DIR));
